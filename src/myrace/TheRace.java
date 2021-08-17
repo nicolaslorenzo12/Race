4a22605db3ad2bac2238d7b831c428e90df70c5a
Nicolas Lorenzo <nicolaslorenzohermosilla@gmail.com>
2021-08-17 08:07:51 +0200

    Changing some words to capital

--- src/myrace/TheRace.java
+++ src/myrace/TheRace.java
@@ -1,22 +1,22 @@
 package myrace;
 
-public class theRace {
+public class TheRace {
     public static void main(String[] args) {
 
         var winner = new Elite("Patricia", "Lorenzo", "Podium");
-        var participant = new Participant[3];
+        var participants = new Participant[3];
 
-        participant[0] = new Participant("Martijn", "Moreel");
-        participant[1] = new Participant("Gabriela", "Lorenzo");
-        participant[2] = winner;
+        participants[0] = new Participant("Martijn", "Moreel");
+        participants[1] = new Participant("Gabriela", "Lorenzo");
+        participants[2] = winner;
 
         var elite = new Elite[2];
 
         elite[0] = new Elite("Nicolas", "Lorenzo", "No Podium");
         elite[1] = new Elite("Angelica", "Guerra", "Podium");
 
-        System.out.println(participant[0].getStatus(2));
-        System.out.println(participant[1].getStatus(1));
+        System.out.println(participants[0].getStatus(2));
+        System.out.println(participants[1].getStatus(1));
         System.out.println( winner.getStatus());
         System.out.println( elite[0].getStatus(1));
         System.out.println(  elite[1].getStatus());
